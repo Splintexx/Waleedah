01d26a3a23de02ff5b17df3414f5443afa4da53e
Splintexx <102814008+Splintexx@users.noreply.github.com>
2025-02-04 03:48:42 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector('.yes-button').addEventListener('click', function() {
+    alert("That's my girl! 💖");
+});
+
+document.querySelector('.no-button').addEventListener('click', function() {
+    alert("Fuck you brudda! 😢");
+});
